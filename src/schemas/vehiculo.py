9adf96156c121b670e8a94a5a6f5467dcc7e77b0
ZeresPrint <aprietom1@upao.edu.pe>
2026-09-20 00:14:13 -0500

    Minor Fix

--- src/schemas/vehiculo.py
+++ src/schemas/vehiculo.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, ConfigDict
+from typing import List
 from src.models.vehiculo import TipoVehiculo
 
 class VehiculoBase(BaseModel):
@@ -15,4 +16,5 @@ class VehiculoImagenLeer(BaseModel):
 class VehiculoLeer(VehiculoBase):
     id: int
     activo: bool
+    imagenes: List[VehiculoImagenLeer] = [] 
     model_config = ConfigDict(from_attributes=True)
